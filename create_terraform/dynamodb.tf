50d0ba0d2df4bc8167bc23e210f1e5436bcefd6d
Carlos Eduardo F <kaduzeiro2013@gmail.com>
2026-02-12 00:51:37 -0300

    Terraforms feitos: Criação do banco dynamo e iam com permissões dos lambdas

---
+++ create_terraform/dynamodb.tf
@@ -0,0 +1,26 @@
+resource "aws_dynamodb_table" "tarefa" {
+  name         = "Tarefa"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key  = "pk"
+  range_key = "sk"
+
+  attribute {
+    name = "pk"
+    type = "S"
+  }
+
+  attribute {
+    name = "sk"
+    type = "S"
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  tags = {
+    Project     = "Tarefa-API"
+    Environment = "dev"
+  }
+}
---
+++ create_terraform/iam.tf
@@ -0,0 +1,43 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "tarefa-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_dynamodb_policy" {
+  name = "tarefa-lambda-dynamodb-policy"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem",
+          "dynamodb:Query",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem"
+        ]
+        Resource = aws_dynamodb_table.tarefa.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
